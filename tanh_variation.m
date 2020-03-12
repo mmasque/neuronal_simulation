d463c86ef8e31b25d966655a2a613320dc9e9280
Marcel <marcel.masques@gmail.com>
2020-03-12 13:15:06 +1100

    Added data generation for different parameters of tanh

--- tanh_variation.m
+++ tanh_variation.m
@@ -13,3 +13,34 @@
 
     
 %}
+
+%% generate data with varying d and a
+as = 0:5;
+ds = 0:0.1:0.5;
+N_BITS = 10^6;
+state_seriess = zeros(N_BITS, numel(as), numel(ds));
+for a_i = 1:numel(as)
+    for d_i = 1:numel(ds)
+        for i = 2:N_BITS
+            % get the past state of the neuron
+            X = state_seriess(i-1, a_i, d_i);
+            a = as(a_i);
+            d = ds(d_i);
+            %calculate probability that in this state the neuron is active
+            %given the activation function parameters and the past state of
+            %the neuron
+            p_active = d * tanh(a * X - 2) + 0.5;
+            
+            %simulate whether the neuron is now active or not. 
+            if rand() < p_active
+                % if it's active, set it to 1, otherwise it's already 0 so
+                % just leave it. 
+                state_seriess(i, a_i, d_i) = 1;
+            end
+            
+        end
+    end
+end
+
+%% run CSSR on all of this data.
+% this'll be slow
